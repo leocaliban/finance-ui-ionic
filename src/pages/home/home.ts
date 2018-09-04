1fa434012e4c7ddc9479ea1ebc9991f066984b3f
Leocaliban <leocaliban@gmail.com>
2018-09-04 06:09:45 -0300

    Cria formulário na tela de pesquisa de lançamentos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, MenuController } from 'ionic-angular';
+import { IonicPage } from 'ionic-angular/navigation/ionic-page';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public menu: MenuController) {
+  }
 
+  ionViewWillEnter() {
+    this.menu.swipeEnable(false);
+  }
+  ionViewDidLeave() {
+    this.menu.swipeEnable(true);
+  }
+
+  public login() {
+    this.navCtrl.setRoot('LancamentosPage');
   }
 
 }
---
+++ src/pages/lancamentos/lancamentos.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-lancamentos',
+  templateUrl: 'lancamentos.html',
+})
+export class LancamentosPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LancamentosPage');
+  }
+
+}
