bd4570e0b0766d1b8feb0f4d2a07908a67a203c0
Leocaliban <leocaliban@gmail.com>
2018-09-17 03:52:01 -0300

    Aplica máscara de dinheiro (brmasker-ionic-3)

--- src/pages/novo-lancamento/novo-lancamento.module.ts
+++ src/pages/novo-lancamento/novo-lancamento.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { NovoLancamentoPage } from './novo-lancamento';
+import { BrMaskerModule } from 'brmasker-ionic-3';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { NovoLancamentoPage } from './novo-lancamento';
   ],
   imports: [
     IonicPageModule.forChild(NovoLancamentoPage),
+    BrMaskerModule
 
   ],
 })
