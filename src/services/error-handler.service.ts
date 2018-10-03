b079110546d624e785a99eb7091d5ec19f5c5739
Leocaliban <leocaliban@gmail.com>
2018-10-03 18:30:09 -0300

    Atualiza o tratamento de erros 4xx

--- src/services/error-handler.service.ts
+++ src/services/error-handler.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ToastController } from 'ionic-angular';
+import { Response } from '@angular/http';
 
 @Injectable()
 export class ErrorHandlerService {
@@ -11,8 +12,18 @@ export class ErrorHandlerService {
 
     if (typeof errorResponse === 'string') {
       mensagem = errorResponse;
+    } else if (errorResponse instanceof Response && errorResponse.status >= 400 && errorResponse.status <= 499) {
+      let erros;
+      mensagem = 'Ocorreu um erro ao processar a sua solicitação.';
+      try {
+        erros = errorResponse.json();
+        mensagem = erros[0].mensagemUsuario;
+      } catch (error) {
+        console.error('Ocorreu um erro.', errorResponse);
+      }
     } else {
       mensagem = 'Desculpe, ocorreu um erro ao processar o serviço remoto.';
+      console.error('Ocorreu um erro.', errorResponse._body);
     }
 
     this.mensagemErro(mensagem);
